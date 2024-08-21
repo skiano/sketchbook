ec1b08c1e03ad7323e62302738910fe44a375340
Greg Skiano <gregoryskiano@gmail.com>
2024-08-20 18:05:08 -0700

    more flapping

--- perch/main.js
+++ perch/main.js
@@ -218,3 +218,245 @@ new p5((p) => {
   };
 
 }, c6);
+
+const c7 = document.createElement('div');
+app.append(c7);
+
+
+new p5((p) => {
+  let birdStrip;
+  let fw = 200;
+  let fh = 200;
+
+  p.preload = () => {
+    birdStrip = p.loadImage('./bird-strip-5.png');
+  }
+
+  p.setup = () => {
+    p.createCanvas(300, 300);
+    p.frameRate(10);
+  }
+
+  p.draw = () => {
+    let f = p.frameCount % 6;
+    let rx = f * fw;
+    p.background('#e8ceb3');
+    p.push();
+    p.blendMode(p.REMOVE);
+    p.image(birdStrip, p.width / 2 - fw / 2, p.height / 2 - fh / 2, fw, fh, rx, 0,fw, fh,);
+    p.pop();
+  };
+
+}, c7);
+
+
+const c8 = document.createElement('div');
+app.append(c8);
+
+new p5((p) => {
+  let birdStrip;
+  let fw = 200;
+  let fh = 200;
+
+  p.preload = () => {
+    birdStrip = p.loadImage('./bird-strip-9.png');
+  }
+
+  p.setup = () => {
+    p.createCanvas(300, 300);
+    p.frameRate(15);
+  }
+
+  p.draw = () => {
+    let f = p.frameCount % 6;
+    let rx = f * fw;
+    p.background('#241a0e');
+    p.push();
+    p.blendMode(p.REMOVE);
+    p.image(birdStrip, p.width / 2 - fw / 2, p.height / 2 - fh / 2, fw, fh, rx, 0,fw, fh,);
+    p.pop();
+  };
+}, c8);
+
+const c9 = document.createElement('div');
+app.append(c9);
+
+new p5((p) => {
+  let birdStrip;
+  let fw = 200;
+  let fh = 200;
+
+  p.preload = () => {
+    birdStrip = p.loadImage('./bird-strip-12.png');
+  }
+
+  p.setup = () => {
+    p.createCanvas(300, 300);
+    p.frameRate(15);
+  }
+
+  p.draw = () => {
+    let f = p.frameCount % 6;
+    let rx = f * fw;
+    p.background('#f4f1ea');
+    p.push();
+    p.image(birdStrip, p.width / 2 - fw / 2, p.height / 2 - fh / 2, fw, fh, rx, 0,fw, fh,);
+    p.pop();
+  };
+}, c9);
+
+const c10 = document.createElement('div');
+app.append(c10);
+
+new p5((p) => {
+  let birdStrip;
+  let fw = 200;
+  let fh = 200;
+
+  p.preload = () => {
+    birdStrip = p.loadImage('./bird-strip-13.png');
+  }
+
+  p.setup = () => {
+    p.createCanvas(300, 300);
+    p.frameRate(15);
+  }
+
+  p.draw = () => {
+    let f = p.frameCount % 7;
+    let rx = f * fw;
+    p.background('#8ba574');
+    p.push();
+    p.image(birdStrip, p.width / 2 - fw / 2, p.height / 2 - fh / 2, fw, fh, rx, 0,fw, fh,);
+    p.pop();
+  };
+}, c10);
+
+
+const c11 = document.createElement('div');
+app.append(c11);
+
+new p5((p) => {
+  let birdStrip;
+  let fw = 200;
+  let fh = 200;
+
+  p.preload = () => {
+    birdStrip = p.loadImage('./bird-strip-14.png');
+  }
+
+  p.setup = () => {
+    p.createCanvas(300, 300);
+    p.frameRate(30);
+  }
+
+  p.draw = () => {
+    let f0 = (p.frameCount % 12) / 2 >> 0;
+    let f1 = ((p.frameCount + 1) % 12) / 2 >> 0;
+
+    // frame blending like...
+    // 00
+    // 01
+    // 11
+
+    let rx = f1 * fw;
+    let rxblend = f0 * fw;
+    p.background('#fff696');
+
+    p.push();
+    p.tint(255, 90);
+    p.image(birdStrip, p.width / 2 - fw / 2, p.height / 2 - fh / 2, fw, fh, rxblend, 0,fw, fh);
+    p.pop();
+
+    p.push();
+    p.image(birdStrip, p.width / 2 - fw / 2, p.height / 2 - fh / 2, fw, fh, rx, 0,fw, fh,);
+    p.pop();
+  };
+}, c11);
+
+const c12 = document.createElement('div');
+app.append(c12);
+
+new p5((p) => {
+  let birdStrip;
+  let fw = 200;
+  let fh = 200;
+
+  p.preload = () => {
+    birdStrip = p.loadImage('./bird-strip-14.png');
+  }
+
+  p.setup = () => {
+    p.createCanvas(300, 300);
+    p.frameRate(30);
+  }
+
+  function drawBird(x, y, offset = 0, scale = 1) {
+    let w = fw * scale;
+    let h = fh * scale;
+    let f = p.frameCount + offset;
+    let f0 = (f % 12) / 2 >> 0;
+    let f1 = ((f + 1) % 12) / 2 >> 0;
+    let rx0 = f0 * fw;
+    let rx1 = f1 * fw;
+
+    p.push();
+    p.tint(255, 90);
+    p.image(birdStrip, x - w/2, y - h/2, w, h, rx0, 0, fw, fh);
+    p.pop();
+
+    p.push();
+    p.image(birdStrip, x - w/2, y - h/2, w, h, rx1, 0, fw, fh);
+    p.pop();
+  }
+
+  p.draw = () => {
+    p.background('#ff8559');
+
+    let dist = p.width + (fw);
+    
+    drawBird((((p.frameCount + 30) * 5) % dist) - fw / 4, 80, 4, 0.4);
+    drawBird(((p.frameCount * 7) % dist) - fw / 4, 150, 1, 0.5);
+    drawBird((((p.frameCount +  50) * 9) % dist) - fw / 4, 200, 3, 0.6);
+    drawBird((((p.frameCount +  10) * 11) % dist) - fw / 4, 250, 5, 0.8);
+  };
+}, c12);
+
+
+const c13 = document.createElement('div');
+app.append(c13);
+
+new p5((p) => {
+  let birdStrip;
+  let fw = 125;
+  let fh = 115;
+
+  p.preload = () => {
+    birdStrip = p.loadImage('./alt-strip.jpg');
+  }
+
+  p.setup = () => {
+    p.createCanvas(300, 300);
+    p.frameRate(30);
+  }
+
+  p.draw = () => {
+    let f = p.frameCount % 16;
+    let rx = (f % 8) * fw;
+    let ry = ((f / 8) >> 0) * fh;
+
+    p.background('#241a0e');
+    p.image(
+      birdStrip,
+      p.width / 2 - fw / 2, // x-coordinate of the top-left corner of the imag
+      p.height / 2 - fh / 2, // y-coordinate of the top-left corner of the image
+      fw, // width to draw the image
+      fh, // height to draw the image
+      rx, // the x-coordinate of the destination rectangle in which to draw the source image
+      ry,  // the y-coordinate of the destination rectangle in which to draw the source image
+      fw, // the width of the destination rectangle
+      fh, // the height of the destination rectangle
+    );
+  };
+
+}, c13);
