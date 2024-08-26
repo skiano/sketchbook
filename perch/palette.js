dfa3abc77fd9432807f1218a323afc9d2dd8e725
Greg Skiano <gregoryskiano@gmail.com>
2024-08-26 16:05:23 -0700

    not sure if this chart thing is getting any better

--- perch/palette.js
+++ perch/palette.js
@@ -52,7 +52,7 @@ function matchLuminance(input, targetLuminance = 0.5) {
 
 const splitAngle = 35;
 const keyLuminance = 0.35;
-const keyColor = matchLuminance(tinycolor('#ff8559').spin(-5), keyLuminance);
+const keyColor = matchLuminance(tinycolor('#ff8559').spin(-4), keyLuminance);
 const complement = keyColor.clone().spin(180);
 const splitA = matchLuminance(complement.clone().spin(-splitAngle), keyColor.getLuminance());
 const splitB = matchLuminance(complement.clone().spin(splitAngle), keyColor.getLuminance());
@@ -62,11 +62,9 @@ const previewPrimary = swatchGroup('Brand Color');
 previewPrimary(keyColor);
 
 const previewSecondary = swatchGroup('Secondary Colors');
-
 previewSecondary(splitA);
 previewSecondary(matchLuminance(splitA.clone().spin(4).desaturate(20), ramp(keyLuminance, 1, 0.5)));
 previewSecondary(matchLuminance(splitA.clone().spin(8).desaturate(25), ramp(keyLuminance, 1, 0.7)));
-
 previewSecondary(matchLuminance(splitB.clone().spin(-4).desaturate(25), ramp(keyLuminance, 1, 0.7)));
 previewSecondary(matchLuminance(splitB.clone().spin(-8).desaturate(20), ramp(keyLuminance, 1, 0.5)));
 previewSecondary(splitB);
@@ -86,26 +84,97 @@ makeGrays(complement, 1.2).forEach(previewGrays2);
 // chart colors
 
 const chart2 = swatchGroup('Chart Colors');
-const previewChartColors = swatchGroup('Chart Colors Expanded');
+const previewChartColors = swatchGroup('Chart Colors (Expanded)');
 
-let movement = splitAngle * 1.12;
-let lumaPattern = [-0.1, -0.18, -0.24];
+// let movement = splitAngle * 1.12;
+// let lumaPattern = [-0.1, -0.18, -0.24];
 
-const chartColors = [
-  complement.clone().spin(-movement * 4),
-  complement.clone().spin(-movement * 3),
-  complement.clone().spin(-movement * 2),
-  complement.clone().spin(-movement * 1),  
-  complement,
-  complement.clone().spin(movement * 1),
-  complement.clone().spin(movement * 2),
-  complement.clone().spin(movement * 3),
-  complement.clone().spin(movement * 4),  
-].map((v, i) => matchLuminance(v.saturate(100 * 1.5 * lumaPattern[i % lumaPattern.length]), keyLuminance + lumaPattern[i % lumaPattern.length]))
+// const chartColors = [
+//   complement.clone().spin(-movement * 4),
+//   complement.clone().spin(-movement * 3),
+//   complement.clone().spin(-movement * 2),
+//   complement.clone().spin(-movement * 1),  
+//   complement,
+//   complement.clone().spin(movement * 1),
+//   complement.clone().spin(movement * 2),
+//   complement.clone().spin(movement * 3),
+//   complement.clone().spin(movement * 4),  
+// ].map((v, i) => matchLuminance(v.saturate(100 * 1.5 * lumaPattern[i % lumaPattern.length]), keyLuminance + lumaPattern[i % lumaPattern.length]))
 
-chartColors.map(previewChartColors);
+// chartColors.map(previewChartColors);
 
 
-chartColors.filter((v, i) => i % 2 === 0).map(chart2);
+// let movement = splitAngle * 0.95;
+// let lumaPattern = [0.1, 0.05, 0.2];
+// const chartColors = [
+//   complement.clone().spin(-movement * 4),
+//   complement.clone().spin(-movement * 3),
+//   complement.clone().spin(-movement * 2),
+//   complement.clone().spin(-movement * 1),  
+//   complement,
+//   complement.clone().spin(movement * 1),
+//   complement.clone().spin(movement * 2),
+//   complement.clone().spin(movement * 3),
+//   complement.clone().spin(movement * 4),  
+// ].map((v, i) => matchLuminance(v.desaturate(0), keyLuminance + lumaPattern[i % lumaPattern.length]))
+
+// chartColors.map(previewChartColors);
+
+// const left = keyColor.clone().spin(-splitAngle * 0.7);
+// const right = keyColor.clone().spin(splitAngle * 0.7);
+
+// const left = complement.clone().spin(-splitAngle * 3.5);
+// const right = complement.clone().spin(splitAngle * 3.5);
+
+
+// const chartColors = [
+//   left,
+//   tinycolor.mix(left, complement, 25),
+//   tinycolor.mix(left, complement, 50),
+//   tinycolor.mix(left, complement, 75),
+//   complement.clone(),
+//   tinycolor.mix(right, complement, 75),
+//   tinycolor.mix(right, complement, 50),
+//   tinycolor.mix(right, complement, 25),
+//   right,
+// ].map((c, i) => {
+
+//   const luma = [
+//     keyLuminance * 2, 
+//     keyLuminance * 1.5, 
+//     keyLuminance * 0.9,
+//   ];
+
+//   return matchLuminance(
+//     c,
+//     luma[i  % luma.length]
+//   );
+// });
+
+// tetrad.map(previewChartColors);
+// tetrad2.map(previewChartColors);
+
+
+// chartColors.filter((v, i) => i % 2 === 0).map(chart2);
+
+const tetrad = complement
+  .tetrad()
+  .map((c, i) => {
+    return matchLuminance(c, i % 2 ? 0.15 : 0.28)
+  });
+
+tetrad.forEach(chart2)
+
+const octad = complement
+  .tetrad()
+  .map((c, i) => {
+    return matchLuminance(c, i % 2 ? 0.18 : 0.28)
+  }).concat(
+    complement.clone().spin(180 / -4).tetrad().map((c, i) => {
+      return matchLuminance(c, i % 2 ? 0.14 : 0.06)
+    })
+  );
+
+octad.forEach(previewChartColors)
 
 
