d34ca050226900e04241367287707c6f3f9e4bd5
Greg Skiano <gregoryskiano@gmail.com>
2024-08-28 19:23:20 -0700

    something pretty cool with rough and shiny matt

--- soft-pattern/alt.js
+++ soft-pattern/alt.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three';
-import exampleTexture from './exampleTexture.js';
+import { createNoise3D } from 'simplex-noise';
+import { exampleTexture, exampleDisplacement, exampleBump, exampleShine } from './exampleTexture.js';
 
+const noise3D = createNoise3D();
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 2000);
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 2000);
 const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
@@ -10,7 +12,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 // renderer.domElement.style.border = '1px solid red'; // preview
 
-camera.position.set(0, 500, 500);
+camera.position.set(0, 200, 0);
 camera.lookAt(0, 0, 0);
 
 const clothWidth = 400;
@@ -21,14 +23,25 @@ const clothGeometry = new THREE.PlaneGeometry(clothWidth, clothHeight, clothSegm
 
 clothGeometry.rotateX(-Math.PI / 2);
 
-const pattern = exampleTexture(clothWidth, clothHeight, '#000');
-// document.body.appendChild(pattern); // preview it
+
+const pattern = exampleTexture(clothWidth * 3, clothHeight * 3, '#000');
+const shine = exampleShine(clothWidth * 3, clothHeight * 3, 40, 245);
+const rough = exampleShine(clothWidth * 3, clothHeight * 3, 245, 40);
+const bmp = exampleBump(clothWidth * 3, clothHeight * 3);
 
 const texture = new THREE.CanvasTexture(pattern);
+const shineMap = new THREE.CanvasTexture(shine);
+const roughMap = new THREE.CanvasTexture(rough);
+const bumpMap = new THREE.CanvasTexture(bmp);
+
 const clothMaterial = new THREE.MeshStandardMaterial({
-  map: texture,         // Apply the texture
-  metalness: 0.0,       // Controls how metallic the surface appears
-  roughness: 0.5,       // Controls the roughness of the surface
+  map: texture,
+  bumpMap: bumpMap,
+  bumpScale: 1, 
+  metalnessMap: shineMap,
+  roughnessMap: roughMap,
+  metalness: 1,       // Controls how metallic the surface appears
+  roughness: 1,       // Controls the roughness of the surface
   side: THREE.DoubleSide // Ensure both sides of the cloth are visible
 });
 const clothMesh = new THREE.Mesh(clothGeometry, clothMaterial);
@@ -36,17 +49,22 @@ clothMesh.receiveShadow = true; // Receive shadows
 clothMesh.castShadow = true;
 scene.add(clothMesh);
 
-const pointLight = new THREE.PointLight(0xffffff, 5);
-pointLight.position.set(0, 30, 0);
-pointLight.castShadow = true;
-scene.add(pointLight);
+// const pointLight = new THREE.PointLight(0xffffff, 5);
+// pointLight.position.set(0, 30, 0);
+// pointLight.castShadow = true;
+// scene.add(pointLight);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 4);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(10, 50, -30);
 scene.add(directionalLight);
+
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, 2);
+directionalLight2.position.set(10, 50, -30);
+directionalLight2.rotateY(Math.PI / 2)
+scene.add(directionalLight2);
 
 // substitute canon here
 const particles = [];
@@ -66,25 +84,28 @@ function animate(currentTime) {
 
   let positionAttribute = clothGeometry.getAttribute('position');
 
-  let  t = currentTime / 10;
+  let  t = currentTime / 18;
 
   // Update cloth vertices based on physics simulation
   for (let i = 0; i < positionAttribute.count; i++) {
     let { x, y, z } = particles[i];
-    positionAttribute.setXYZ(i, x, y + (Math.cos((z + t) / 8) * 3), z  );
+    // positionAttribute.setXYZ(i, x, y + (Math.cos((z + t) / 12) * 2) + (Math.cos((x + (t / 2)) / 16) * 1), z  );
+    let ny = noise3D(x / 80, z / 80, t / 80) * 4;
+    let sy = Math.cos((z + t) / 20) * 4;
+    positionAttribute.setXYZ(i, x, y + ny + sy, z);
   }
 
-  // clothGeometry.geometry.computeBoundingSphere()
   clothGeometry.computeBoundingBox();
   clothGeometry.computeBoundingSphere();
   clothGeometry.computeVertexNormals();
   positionAttribute.needsUpdate = true;
 
-  const radius = 500;
-  const angle = currentTime * 0.0001; // Adjust the multiplier to control speed
+  const radius = 130;
+  const angle = currentTime * 0.0004; // Adjust the multiplier to control speed
   // Update camera position to orbit around the Y-axis
   camera.position.x = radius * Math.cos(angle);
   camera.position.z = radius * Math.sin(angle);
+  camera.position.y = radius;
   // Ensure the camera is always looking at the center (where the cloth is)
   camera.lookAt(0, 0, 0);
 
--- soft-pattern/exampleTexture.js
+++ soft-pattern/exampleTexture.js
@@ -1,4 +1,5 @@
-export default function exampleTexture(w, h, bg = '#fff') {
+import { createNoise3D, createNoise2D } from 'simplex-noise';
+export function exampleTexture(w, h, bg = '#fff') {
   const canvas = document.createElement('canvas');
   canvas.ctx = canvas.getContext('2d', { alpha: false });
   canvas.width = w;
@@ -21,6 +22,148 @@ export default function exampleTexture(w, h, bg = '#fff') {
     data[i] = (x / 12) % 2 ? a : 0; // red
     data[i + 1] = (y / 12) % 2 ? b : 0; // green
     data[i + 2] = (x / 24) % 2 ? c : 0; // blue
+    x += 1;
+    if (x >= canvas.width) {
+      x = 0;
+      y += 1;
+    }
+  }
+  canvas.ctx.putImageData(imageData, 0, 0);
+
+  // end example
+
+  return canvas;
+}
+
+export function exampleShine(w, h, bg, fg) {
+  const canvas = document.createElement('canvas');
+  canvas.ctx = canvas.getContext('2d', { alpha: false });
+  canvas.width = w;
+  canvas.height = h;
+  canvas.style.width = `${w / 2}px`;
+  canvas.style.height = `${h / 2}px`;
+  canvas.ctx.fillStyle = bg;
+  canvas.ctx.fillRect(0, 0, w, h);
+
+  // some dumb pattern thing...
+
+  const imageData = canvas.ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] = (x / 12) % 2 ? bg : fg; // red
+    data[i + 1] = (y / 12) % 2 ? bg : fg; // green
+    data[i + 2] = (x / 24) % 2 ? bg : fg; // blue
+    x += 1;
+    if (x >= canvas.width) {
+      x = 0;
+      y += 1;
+    }
+  }
+  canvas.ctx.putImageData(imageData, 0, 0);
+
+  // end example
+
+  return canvas;
+}
+
+
+
+export function exampleDisplacement(w, h) {
+  const canvas = document.createElement('canvas');
+  canvas.ctx = canvas.getContext('2d', { alpha: false });
+  canvas.width = w;
+  canvas.height = h;
+  canvas.style.width = `${w / 2}px`;
+  canvas.style.height = `${h / 2}px`;
+  canvas.ctx.fillStyle = '#000';
+  canvas.ctx.fillRect(0, 0, w, h);
+
+  // some dumb pattern thing...
+
+  const imageData = canvas.ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < data.length; i += 4) {
+    let g = y / canvas.height * 255;
+    data[i] = g; // red
+    data[i + 1] = g; // green
+    data[i + 2] = g; // blue
+    x += 1;
+    if (x >= canvas.width) {
+      x = 0;
+      y += 1;
+    }
+  }
+  canvas.ctx.putImageData(imageData, 0, 0);
+
+  // end example
+
+  return canvas;
+}
+
+
+export function exampleBump(w, h) {
+  const canvas = document.createElement('canvas');
+  canvas.ctx = canvas.getContext('2d', { alpha: false });
+  canvas.width = w;
+  canvas.height = h;
+  canvas.style.width = `${w / 2}px`;
+  canvas.style.height = `${h / 2}px`;
+  canvas.ctx.fillStyle = '#000';
+  canvas.ctx.fillRect(0, 0, w, h);
+
+  const noise = createNoise2D();
+
+  // some dumb pattern thing...
+
+  const imageData = canvas.ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < data.length; i += 4) {
+    let g = ((noise(x * 10, y * 10) + 1) / 2) * 255;
+    data[i] = g; // red
+    data[i + 1] = g; // green
+    data[i + 2] = g; // blue
+    x += 1;
+    if (x >= canvas.width) {
+      x = 0;
+      y += 1;
+    }
+  }
+  canvas.ctx.putImageData(imageData, 0, 0);
+
+  // end example
+
+  return canvas;
+}
+
+export function exampleCloud(w, h) {
+  const canvas = document.createElement('canvas');
+  canvas.ctx = canvas.getContext('2d', { alpha: false });
+  canvas.width = w;
+  canvas.height = h;
+  canvas.style.width = `${w / 2}px`;
+  canvas.style.height = `${h / 2}px`;
+  canvas.ctx.fillStyle = '#000';
+  canvas.ctx.fillRect(0, 0, w, h);
+
+  // some dumb pattern thing...
+
+  const noise = createNoise3D();
+
+  const imageData = canvas.ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < data.length; i += 4) {
+    let g = ((noise(x / 150, y / 150, 1) + 1) / 2) * 255;
+    data[i] = g; // red
+    data[i + 1] = g; // green
+    data[i + 2] = g; // blue
     x += 1;
     if (x >= canvas.width) {
       x = 0;
