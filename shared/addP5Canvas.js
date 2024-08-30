bd819fc9349df5b5a29b02e2b9267ca33a86dc8c
Greg Skiano <gregoryskiano@gmail.com>
2024-08-30 13:05:27 -0700

    thinking more about grid pattern api and how it relates to looping

---
+++ shared/addP5Canvas.js
@@ -0,0 +1,34 @@
+import p5 from 'p5';
+
+// this is super silly,
+// but it allows you to add setup steps AFTER the _standardSetup runs...
+p5.prototype.registerMethod('beforeSetup', function () {
+  let userSetup = this.setup;
+  this.setup = function () {
+    this._standardSetup();
+    if (userSetup) userSetup();
+  }
+});
+
+export default function addCanvas(fn, opt) {
+  opt = {
+    fps: 15,
+    width: 540,
+    height: 540,
+    rootId: 'app',
+    background: '#eee',
+    ...opt,
+  }
+  const c = document.createElement('div');
+  c.style.width = `${opt.width}px`;
+  c.style.height = `${opt.height}px`;
+  c.style.background = opt.background;
+  document.getElementById(opt.rootId).append(c);
+  new p5((p) => {
+    p._standardSetup = () => {
+      p.createCanvas(opt.width, opt.height);
+      p.frameRate(opt.fps);
+    }
+    fn(p);
+  }, c);
+}
---
+++ shared/p5Loop.js
@@ -0,0 +1,25 @@
+// @see https://github.com/processing/p5.js/blob/main/contributor_docs/creating_libraries.md#step-6
+
+// USAGE:
+// in setup() call loopLength() with the number of frames the loop should take
+// then, inside draw(), you can access loopFrame, loopFraction, and loopAngle (radians)
+
+export default function p5Loop(p5) {
+  p5.prototype.loopLength = function(v) {
+    this._loopLength = v;
+  }
+
+  p5.prototype.loopFrame = function(v) {
+    return (this.frameCount - 1) % this._loopLength;
+  }
+
+  p5.prototype.registerMethod('pre', function () {
+    // if there is no loopLength set... do not incur the extra calculations
+    if (!this._loopLength) return;
+
+    // NOTE: the -1 here is because frameCount begins at 1 with p5 (not helpful for using %)
+    this.loopFrame = (this.frameCount - 1) % this._loopLength;
+    this.loopFraction = this.loopFrame / this._loopLength;
+    this.loopAngle = this.loopFraction * this.TAU;
+  });
+}
